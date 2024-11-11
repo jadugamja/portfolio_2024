29f8b9af20a4babfbc941927de2f98e2fd882515
jasook <gheddong2@naver.com>
2024-11-11 14:30:27 +0900

    Test: Vite로 빌드 후 배포

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/portfolio_2024/',
   plugins: [react()],
   resolve: {
     alias: {
