49b9c83aab3e49a70614cf08e101a8af57040d44
jasook <gheddong2@naver.com>
2024-11-28 14:38:47 +0900

    Feat: project description 컴포넌트에 쓰이는 타입 정의

---
+++ src/types/project.d.ts
@@ -0,0 +1,10 @@
+export type Children = {
+  children: React.ReactNode;
+};
+
+export type SummaryProps = {
+  period: string;
+  contribution: string;
+  participants: string;
+  skills: string;
+};
